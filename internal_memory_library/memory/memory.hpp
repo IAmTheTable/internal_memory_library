e7ace69a67774e5303eadedba45bd875b69bd001
Jayden <53480580+AmJayden@users.noreply.github.com>
2021-08-12 16:46:08 -0400

    rewrite to actually work, and make smaller.

--- internal_memory_library/memory/memory.hpp
+++ internal_memory_library/memory/memory.hpp
@@ -4,29 +4,31 @@
 
 class memory
 {
-	std::uintptr_t current_handle;
+	// provide default initialization for current_handle
+	std::uintptr_t current_handle = 0;
 
 public:
 
-	memory(std::string&& mod) { current_handle = reinterpret_cast<std::uintptr_t>(GetModuleHandleA(mod.c_str())); };
+	// rvalue usage on std::string should not be permitted nor used here, if c_str is going to be substituted.
+	// initialization of current_handle can be in the initializer list for the constructor
+	memory(const char* const mod) : current_handle{ reinterpret_cast<std::uintptr_t>(GetModuleHandleA(mod)) } {};
+
+	// current_handle has no default value, if constructed with no parameters it will be undefined.
 	memory() = default;
 
+	// this will not work, it will need to be casted twice in order to PROPERLY use this function
+	// will break on sizes < sizeof(void*)
 	template<class T>
 	T read(std::uintptr_t address)
 	{
-		if (current_handle)
-			return T(reinterpret_cast<T*>(current_handle + address));
-
-		return T(reinterpret_cast<T*>(address));
+		return *reinterpret_cast<T*>(current_handle ? current_handle + address : address);
 	}
 
+	// comply with the read definition, and fix for pointers as well
+	// &address is not valid in this scenario.
 	template<class T>
-	void write(void* address, T value)
+	void write(std::uintptr_t address, T value)
 	{
-
-		if (current_handle)
-			*reinterpret_cast<T*>(current_handle + reinterpret_cast<std::uintptr_t>(&address)) = value;
-
-		*reinterpret_cast<T*>(address) = value;
+		*reinterpret_cast<T*>(current_handle ? current_handle + address : address) = value;
 	}
 };
